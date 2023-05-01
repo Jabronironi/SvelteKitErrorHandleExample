4b7ede4c556cbfb3d619759d73f45371b2e90b8f
Jeremy Armstrong <jeremy.armstrong@solidstateops.com>
2023-05-01 11:14:29 -0600

    Trying to use the window error handler.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+    let handleError = () => {
+        console.log('Error Handler Fired');
+    };
+</script>
+<svelte:window on:error={handleError}/>
 <h1>Example App</h1>
 <slot />
