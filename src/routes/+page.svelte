a6928437ee5dc8157bb48140371bd2ebfdd36b2d
Jeremy Armstrong <jeremy.armstrong@solidstateops.com>
2023-05-02 17:10:50 -0600

    chart js example.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,67 @@
-<script type="ts">
-    import { browser } from "$app/environment";
-    server.blowup();
-    if (browser) {
-        client.blowup();
-    }
+<script type="ts">    
+    import { Line } from "svelte-chartjs";
+    import {Chart as ChartJS, registerables} from 'chart.js'    
+    ChartJS.register(...registerables);
+    let data = {
+        labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+        ],
+        datasets: [
+            {
+                label: "My First dataset",
+                fill: true,
+                lineTension: 0.3,
+                backgroundColor: "rgba(225, 204,230, .3)",
+                borderColor: "rgb(205, 130, 158)",
+                borderCapStyle: "butt",
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: "miter",
+                pointBorderColor: "rgb(205, 130,1 58)",
+                pointBackgroundColor: "rgb(255, 255, 255)",
+                pointBorderWidth: 11,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgb(0, 0, 0)",
+                pointHoverBorderColor: "rgba(220, 220, 220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40],
+            },
+            {
+                label: "My Second dataset",
+                fill: true,
+                lineTension: 0.3,
+                backgroundColor: "rgba(184, 185, 210, .3)",
+                borderColor: "rgb(35, 26, 136)",
+                borderCapStyle: "butt",
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: "miter",
+                pointBorderColor: "rgb(35, 26, 136)",
+                pointBackgroundColor: "rgb(255, 255, 255)",
+                pointBorderWidth: 10,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgb(0, 0, 0)",
+                pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [28, 48, 40, 19, 86, 27, 90],
+            },
+        ],
+    };
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+    documentation
+    <Line data="{data}" />
 </p>
